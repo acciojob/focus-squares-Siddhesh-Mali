8ee9aedd5b99c93293451910d2b01ae12981c8c0
Siddhesh-Mali <129844841+Siddhesh-Mali@users.noreply.github.com>
2024-02-26 15:50:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+// const squares = document.querySelectorAll(".square");
+
+// squares.forEach(quare => {
+//   quare.addEventListener("mouseover", () => {
+//     squares.forEach(otherSquare => {
+//       if (otherSquare !== quare) {
+//         otherSquare.style.backgroundColor = "#6F4E37"; /* Coffee */
+//       }
+//     });
+//   });
+
+//   quare.addEventListener("mouseout", () => {
+//     squares.forEach(otherSquare => {
+//       otherSquare.style.backgroundColor = "#E6E6FA"; /* Lavender */
+//     });
+//   });
+// });
+
+const squares = document.querySelectorAll(".square");
+
+squares.forEach(square => {
+  square.addEventListener("mouseover", () => {
+    squares.forEach(otherSquare => {
+      otherSquare.classList.toggle("coffee", otherSquare !== square);
+    });
+  });
+});
